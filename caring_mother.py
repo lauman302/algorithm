8a1cf179cc55b37190b4ba2f60cbccc118234386
Rustam A <69015050+lauman302@users.noreply.github.com>
2022-04-17 17:15:04 +0300

    new

---
+++ caring_mother.py
@@ -0,0 +1,23 @@
+# Comment it before submitting
+#class Node:  
+#    def __init__(self, value, next_item=None):  
+#        self.value = value  
+#        self.next_item = next_item
+
+def solution(node, elem):
+    idx = 0
+    while node:
+        if node.value == elem:
+            return idx
+        else:
+            node = node.next_item
+            idx += 1
+    return -1
+
+def test():
+    node3 = Node("node3", None)
+    node2 = Node("node2", node3)
+    node1 = Node("node1", node2)
+    node0 = Node("node0", node1)
+    idx = solution(node0, "node2")
+    # result is idx == 2
