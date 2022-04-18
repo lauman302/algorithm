c5a28296da612f8b621a89663359e9db614ba16c
Rustam <xrio90@gmail.com>
2022-04-18 21:44:35 +0300

    add new tasks

--- StackMax.py
+++ StackMax.py
@@ -1,3 +1,13 @@
+'''
+Формат ввода
+В первой строке записано одно число n — количество команд, которое не превосходит 10000. В следующих n строках идут команды. Команды могут быть следующих видов:
+
+push(x) — добавить число x в стек;
+pop() — удалить число с вершины стека;
+get_max() — напечатать максимальное число в стеке;
+Если стек пуст, при вызове команды get_max() нужно напечатать «None», для команды pop() — «error».
+'''
+
 class StackMax:
     def __init__(self):
         self.items = []
@@ -36,5 +46,5 @@ for i in range(cnt_commands):
         if stack.pop() == 'error':
             result.append('error')
     if command[0] == 'get_max':
-        result.append(stack.get_max)
+        result.append(stack.get_max())
 print(*result, sep='\n')
