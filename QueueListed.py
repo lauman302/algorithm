b94d27e6fa2bd2cc68565f9bb85e250b6e10d87e
Rustam <xrio90@gmail.com>
2022-04-22 01:04:11 +0300

    add new

--- QueueListed.py
+++ QueueListed.py
@@ -40,3 +40,62 @@ size
 get
 get
 '''
+
+class QueueListed:
+    class Node:
+        def __init__(self, value=None, next=None):
+            self.value = value
+            self.next = next
+        
+        def __str__(self):
+            return self.value
+
+    def __init__(self):
+        self.queue = []
+        self.head = self.Node()
+        self.tail = self.Node()
+        self.size = 0
+
+    def isEmpty(self):
+        return self.queue == 0
+    
+    def get(self):
+        if self.isEmpty():
+            return 'error'
+        value = self.queue.pop(self.head)
+        self.head += 1
+        return value
+        
+
+    def put(self, item):
+        if self.isEmpty():
+            self.queue[self.tail] = self.Node(item)
+            self.tail += 1
+            self.size += 1
+        else:
+            prev_node = self.queue[self.tail - 1]
+            new_node = self.Node(item)
+            prev_node.next = new_node
+            self.queue[self.tail] = new_node
+
+
+    def size(self):
+        return self.size
+
+if __name__ == '__main__':
+    result = []
+    cnt_commands = int(input())
+    max_size = int(input())
+    queue = MyQueueSized(max_size)
+    for i in range(cnt_commands):
+        command = input().split()
+        if command[0] == 'push':
+            if queue.push(command[1]) == 'error':
+                result.append('error')
+        elif command[0] == 'pop':
+            result.append(queue.pop())
+        elif command[0] == 'peek':
+            result.append(queue.peek())
+        elif command[0] == 'size':
+            result.append(queue.size)
+    print(*result, sep='\n')
