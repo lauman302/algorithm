3777c4be69a1169c13ba5d10ba14ce9f59ea3c18
Rustam <xrio90@gmail.com>
2022-04-24 23:12:59 +0300

    add calculator

--- calculator.py
+++ calculator.py
@@ -68,3 +68,49 @@
 19:24
 значение сохраняешь в [-2] а последний элемент pop()
 '''
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+    
+    def push(self, value):
+        self.items.append(value)
+    
+    def pop(self):
+        return self.items.pop()
+
+
+def calculate(line):
+    for symbol in line:
+        if symbol == '+':
+            first, two = stack.items[-2:]
+            result = first + two
+            stack.pop(), stack.pop()
+            stack.push(result)
+        elif symbol == '-':
+            first, two = stack.items[-2:]
+            result = first - two
+            stack.pop(), stack.pop()
+            stack.push(result)
+        elif symbol == '*':
+            first, two = stack.items[-2:]
+            result = first * two
+            stack.pop(), stack.pop()
+            stack.push(result)            
+        elif symbol == '/':
+            first, two = stack.items[-2:]
+            result = first // two
+            stack.pop(), stack.pop()
+            stack.push(result)
+        else:
+            stack.push(int(symbol))
+    return stack.items[-1]
+
+
+if __name__ == '__main__':
+    stack = Stack()
+    line = input().split()
+    print(calculate(line))
